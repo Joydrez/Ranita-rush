c50531e57d2a48497661e20f451e333ff5d54e00
Joydrez <jhernandezchavarria@gmail.com>
2024-03-04 21:58:34 -0600

    Agregar archivos de proyecto.

---
+++ Ranita rush/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace Ranita_rush
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tmrCar1 = new System.Windows.Forms.Timer(this.components);
+            this.pBoxCar6 = new System.Windows.Forms.PictureBox();
+            this.pBoxCar5 = new System.Windows.Forms.PictureBox();
+            this.pBoxCar4 = new System.Windows.Forms.PictureBox();
+            this.pBoxCar3 = new System.Windows.Forms.PictureBox();
+            this.pBoxCar2 = new System.Windows.Forms.PictureBox();
+            this.pBoxCar1 = new System.Windows.Forms.PictureBox();
+            this.pBoxRana = new System.Windows.Forms.PictureBox();
+            this.tmrCar2 = new System.Windows.Forms.Timer(this.components);
+            this.tmrCar3 = new System.Windows.Forms.Timer(this.components);
+            this.tmrCar4 = new System.Windows.Forms.Timer(this.components);
+            this.tmrCar5 = new System.Windows.Forms.Timer(this.components);
+            this.tmrCar6 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxCar6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxCar5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxCar4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxCar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxCar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxCar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxRana)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tmrCar1
+            // 
+            this.tmrCar1.Interval = 50;
+            this.tmrCar1.Tick += new System.EventHandler(this.tmrCar1_Tick);
+            // 
+            // pBoxCar6
+            // 
+            this.pBoxCar6.Image = global::Ranita_rush.Properties.Resources.piteroleft;
+            this.pBoxCar6.Location = new System.Drawing.Point(1109, 89);
+            this.pBoxCar6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pBoxCar6.Name = "pBoxCar6";
+            this.pBoxCar6.Size = new System.Drawing.Size(107, 62);
+            this.pBoxCar6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBoxCar6.TabIndex = 6;
+            this.pBoxCar6.TabStop = false;
+            // 
+            // pBoxCar5
+            // 
+            this.pBoxCar5.Image = global::Ranita_rush.Properties.Resources.piteroleft;
+            this.pBoxCar5.Location = new System.Drawing.Point(1109, 162);
+            this.pBoxCar5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pBoxCar5.Name = "pBoxCar5";
+            this.pBoxCar5.Size = new System.Drawing.Size(107, 62);
+            this.pBoxCar5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBoxCar5.TabIndex = 5;
+            this.pBoxCar5.TabStop = false;
+            // 
+            // pBoxCar4
+            // 
+            this.pBoxCar4.Image = global::Ranita_rush.Properties.Resources.piteroright;
+            this.pBoxCar4.Location = new System.Drawing.Point(13, 236);
+            this.pBoxCar4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pBoxCar4.Name = "pBoxCar4";
+            this.pBoxCar4.Size = new System.Drawing.Size(107, 62);
+            this.pBoxCar4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBoxCar4.TabIndex = 4;
+            this.pBoxCar4.TabStop = false;
+            // 
+            // pBoxCar3
+            // 
+            this.pBoxCar3.Image = global::Ranita_rush.Properties.Resources.piteroright;
+            this.pBoxCar3.Location = new System.Drawing.Point(13, 384);
+            this.pBoxCar3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pBoxCar3.Name = "pBoxCar3";
+            this.pBoxCar3.Size = new System.Drawing.Size(107, 62);
+            this.pBoxCar3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBoxCar3.TabIndex = 3;
+            this.pBoxCar3.TabStop = false;
+            // 
+            // pBoxCar2
+            // 
+            this.pBoxCar2.Image = global::Ranita_rush.Properties.Resources.piteroleft;
+            this.pBoxCar2.Location = new System.Drawing.Point(1109, 458);
+            this.pBoxCar2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pBoxCar2.Name = "pBoxCar2";
+            this.pBoxCar2.Size = new System.Drawing.Size(107, 62);
+            this.pBoxCar2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBoxCar2.TabIndex = 2;
+            this.pBoxCar2.TabStop = false;
+            // 
+            // pBoxCar1
+            // 
+            this.pBoxCar1.Image = global::Ranita_rush.Properties.Resources.piteroright;
+            this.pBoxCar1.Location = new System.Drawing.Point(13, 532);
+            this.pBoxCar1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pBoxCar1.Name = "pBoxCar1";
+            this.pBoxCar1.Size = new System.Drawing.Size(107, 62);
+            this.pBoxCar1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBoxCar1.TabIndex = 1;
+            this.pBoxCar1.TabStop = false;
+            // 
+            // pBoxRana
+            // 
+            this.pBoxRana.Image = global::Ranita_rush.Properties.Resources.Frog_back;
+            this.pBoxRana.Location = new System.Drawing.Point(580, 606);
+            this.pBoxRana.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pBoxRana.Name = "pBoxRana";
+            this.pBoxRana.Size = new System.Drawing.Size(80, 62);
+            this.pBoxRana.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBoxRana.TabIndex = 0;
+            this.pBoxRana.TabStop = false;
+            // 
+            // tmrCar2
+            // 
+            this.tmrCar2.Interval = 2;
+            this.tmrCar2.Tick += new System.EventHandler(this.tmrCar2_Tick);
+            // 
+            // tmrCar3
+            // 
+            this.tmrCar3.Interval = 7;
+            this.tmrCar3.Tick += new System.EventHandler(this.tmrCar3_Tick);
+            // 
+            // tmrCar4
+            // 
+            this.tmrCar4.Interval = 1;
+            this.tmrCar4.Tick += new System.EventHandler(this.tmrCar4_Tick);
+            // 
+            // tmrCar5
+            // 
+            this.tmrCar5.Interval = 15;
+            this.tmrCar5.Tick += new System.EventHandler(this.tmrCar5_Tick);
+            // 
+            // tmrCar6
+            // 
+            this.tmrCar6.Interval = 3;
+            this.tmrCar6.Tick += new System.EventHandler(this.tmrCar6_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1232, 690);
+            this.Controls.Add(this.pBoxCar6);
+            this.Controls.Add(this.pBoxCar5);
+            this.Controls.Add(this.pBoxCar4);
+            this.Controls.Add(this.pBoxCar3);
+            this.Controls.Add(this.pBoxCar2);
+            this.Controls.Add(this.pBoxCar1);
+            this.Controls.Add(this.pBoxRana);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.RightToLeftLayout = true;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Frogger";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxCar6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxCar5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxCar4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxCar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxCar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxCar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxRana)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pBoxRana;
+        private System.Windows.Forms.PictureBox pBoxCar1;
+        private System.Windows.Forms.Timer tmrCar1;
+        private System.Windows.Forms.PictureBox pBoxCar2;
+        private System.Windows.Forms.PictureBox pBoxCar3;
+        private System.Windows.Forms.PictureBox pBoxCar4;
+        private System.Windows.Forms.PictureBox pBoxCar5;
+        private System.Windows.Forms.PictureBox pBoxCar6;
+        private System.Windows.Forms.Timer tmrCar2;
+        private System.Windows.Forms.Timer tmrCar3;
+        private System.Windows.Forms.Timer tmrCar4;
+        private System.Windows.Forms.Timer tmrCar5;
+        private System.Windows.Forms.Timer tmrCar6;
+    }
+}
+
---
+++ Ranita rush/Form1.cs
@@ -0,0 +1,431 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ranita_rush
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            int xRana, yRana;
+            xRana = pBoxRana.Location.X;
+            yRana = pBoxRana.Location.Y;
+
+            if (e.KeyChar==119)
+            {
+                pBoxRana.Location = new Point(xRana, yRana - 60);
+                pBoxRana.Image = Image.FromFile(@"C:\Users\johan\source\repos\Ranita rush\Ranita rush\bin\Debug\Sprays ranita rush\Frog_back.png");
+            }
+            else if (e.KeyChar == 115)
+            {
+                pBoxRana.Location = new Point(xRana, yRana + 60);
+                pBoxRana.Image = Image.FromFile(@"C:\Users\johan\source\repos\Ranita rush\Ranita rush\bin\Debug\Sprays ranita rush\Frog.png");
+            }
+            else if (e.KeyChar == 97)
+            {
+                pBoxRana.Location = new Point(xRana - 60, yRana);
+            }
+            else if (e.KeyChar == 100)
+            {
+                pBoxRana.Location = new Point(xRana + 60, yRana);
+            }
+
+
+            if (pBoxRana.Location.X+79>=940)
+            {
+                pBoxRana.Location = new Point(xRana, yRana);
+            }
+            if (pBoxRana.Location.X<=9)
+            {
+                pBoxRana.Location = new Point(xRana, yRana);
+            }
+            if (pBoxRana.Location.Y <= 5)
+            {
+                pBoxRana.Location = new Point(xRana, yRana);
+            }
+            if (pBoxRana.Location.Y + 50 >= 670)
+            {
+                pBoxRana.Location = new Point(xRana, yRana);
+            }
+        }
+
+        public void tmrCar1_Tick(object sender, EventArgs e)
+        {
+            int Carx1 = pBoxCar1.Location.X, Cary1 = pBoxCar1.Location.Y;                                                            
+            bool avanzar = true, dead = false;
+            int hitboxRanaY1 = pBoxRana.Location.Y, hitboxRanaY2 = pBoxRana.Location.Y + 55;
+            int hitboxRanaX1 = pBoxRana.Location.X, hitboxRanaX2 = pBoxRana.Location.X + 80;
+
+            tmrCar1.Interval = 50;
+            int hitboxCarro1Y1 = pBoxCar1.Location.Y, hitboxCarro1Y2 = pBoxCar1.Location.Y + 50;
+            int hitboxCarro1X1 = pBoxCar1.Location.X, hitboxCarro1X2 = pBoxCar1.Location.X + 80;                                           
+
+            if (avanzar)
+            {
+                pBoxCar1.Location = new Point(Carx1 + 5, Cary1);                
+
+                if (Carx1 + 90 >= 1000)
+                {
+                    avanzar = false;
+                }
+            }
+
+            //hitbox1
+            if (hitboxRanaX1 <= hitboxCarro1X2)
+            {
+                if (hitboxRanaY1 <= hitboxCarro1Y2)
+                {
+                    if (hitboxRanaX2 >= hitboxCarro1X1)
+                    {
+                        if (hitboxRanaY2 >= hitboxCarro1Y2)
+                        {
+                            avanzar = false;
+                            dead = true;
+                        }
+                    }
+                }              
+            }                                                            
+            //ciclo autos
+            if (avanzar == false)
+            {
+                if (dead == false)
+                {
+                    pBoxCar1.Location = new Point(-90, Cary1);                    
+                    avanzar = true;                    
+                }
+                else if (dead == true)
+                {
+                    tmrCar1.Enabled = false;                    
+                    pBoxRana.Image = Image.FromFile(@"C:\Users\johan\source\repos\Ranita rush\Ranita rush\Resources\Frogdead.png");
+                    
+                }
+            }
+            if (tmrCar1.Enabled == false)
+            {
+                tmrCar2.Enabled = false;
+                tmrCar3.Enabled = false;
+                tmrCar4.Enabled = false;
+                tmrCar5.Enabled = false;
+                tmrCar6.Enabled = false;
+            }
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            int x = pBoxCar1.Location.X, y = pBoxCar1.Location.Y;
+
+            tmrCar1.Enabled = true;
+            tmrCar2.Enabled = true;
+            tmrCar3.Enabled = true;
+            tmrCar4.Enabled = true;
+            tmrCar5.Enabled = true;
+            tmrCar6.Enabled = true;
+                       
+        }
+        public void tmrCar2_Tick(object sender, EventArgs e)
+        {
+            int Carx2 = pBoxCar2.Location.X, Cary2 = pBoxCar2.Location.Y;
+            int hitboxCarro2Y1 = pBoxCar2.Location.Y, hitboxCarro2Y2 = pBoxCar2.Location.Y + 50;
+            int hitboxCarro2X1 = pBoxCar2.Location.X, hitboxCarro2X2 = pBoxCar2.Location.X + 80;
+            bool avanzar = true, dead = false;
+            int hitboxRanaY1 = pBoxRana.Location.Y, hitboxRanaY2 = pBoxRana.Location.Y + 55;
+            int hitboxRanaX1 = pBoxRana.Location.X, hitboxRanaX2 = pBoxRana.Location.X + 80;
+            tmrCar2.Interval = 1;
+
+            if (avanzar)
+            {
+                pBoxCar2.Location = new Point(Carx2 - 5, Cary2);
+
+                if (Carx2 <= 0)
+                {
+                    avanzar = false;
+                }
+            }
+            //hitbox2
+            if (hitboxRanaX1 <= hitboxCarro2X2)
+            {
+                if (hitboxRanaY1 <= hitboxCarro2Y2)
+                {
+                    if (hitboxRanaX2 >= hitboxCarro2X1)
+                    {
+                        if (hitboxRanaY2 >= hitboxCarro2Y2)
+                        {
+                            avanzar = false;
+                            dead = true;
+                        }
+                    }
+                }
+            }
+            //ciclo autos
+            if (avanzar == false)
+            {
+                if (dead == false)
+                {
+                    pBoxCar2.Location = new Point(900, Cary2);
+                    avanzar = true;
+                }
+                else if (dead == true)
+                {
+                    tmrCar2.Enabled = false;
+                    pBoxRana.Image = Image.FromFile(@"C:\Users\johan\source\repos\Ranita rush\Ranita rush\Resources\Frogdead.png");
+                }
+            }
+            if (tmrCar2.Enabled == false)
+            {
+                tmrCar1.Enabled = false;
+                tmrCar3.Enabled = false;
+                tmrCar4.Enabled = false;
+                tmrCar5.Enabled = false;
+                tmrCar6.Enabled = false;
+            }
+        }
+
+        public void tmrCar3_Tick(object sender, EventArgs e)
+        {
+            int Carx3 = pBoxCar3.Location.X, Cary3 = pBoxCar3.Location.Y;
+
+            int hitboxCarro3Y1 = pBoxCar3.Location.Y, hitboxCarro3Y2 = pBoxCar3.Location.Y + 50;
+            int hitboxCarro3X1 = pBoxCar3.Location.X, hitboxCarro3X2 = pBoxCar3.Location.X + 80;
+            bool avanzar = true, dead = false;
+            int hitboxRanaY1 = pBoxRana.Location.Y, hitboxRanaY2 = pBoxRana.Location.Y + 55;
+            int hitboxRanaX1 = pBoxRana.Location.X, hitboxRanaX2 = pBoxRana.Location.X + 80;
+            tmrCar3.Interval = 60;
+
+            if (avanzar)
+            {
+                pBoxCar3.Location = new Point(Carx3 + 5, Cary3);
+
+                if (Carx3 + 90 >= 1000)
+                {
+                    avanzar = false;
+                }
+            }
+            //hitbox3
+            if (hitboxRanaX1 <= hitboxCarro3X2)
+            {
+                if (hitboxRanaY1 <= hitboxCarro3Y2)
+                {
+                    if (hitboxRanaX2 >= hitboxCarro3X1)
+                    {
+                        if (hitboxRanaY2 >= hitboxCarro3Y2)
+                        {
+                            avanzar = false;
+                            dead = true;
+                        }
+                    }
+                }
+            }
+            //ciclo autos
+            if (avanzar == false)
+            {
+                if (dead == false)
+                {
+                    pBoxCar3.Location = new Point(-90, Cary3);
+                    avanzar = true;
+                }
+                else if (dead == true)
+                {
+                    tmrCar3.Enabled = false;
+                    pBoxRana.Image = Image.FromFile(@"C:\Users\johan\source\repos\Ranita rush\Ranita rush\Resources\Frogdead.png");
+                }
+            }
+            if (tmrCar3.Enabled == false)
+            {
+                tmrCar1.Enabled = false;
+                tmrCar2.Enabled = false;
+                tmrCar4.Enabled = false;
+                tmrCar5.Enabled = false;
+                tmrCar6.Enabled = false;
+            }
+        }
+
+        public void tmrCar4_Tick(object sender, EventArgs e)
+        {
+            int Carx4 = pBoxCar4.Location.X, Cary4 = pBoxCar4.Location.Y;
+
+            int hitboxCarro4Y1 = pBoxCar4.Location.Y, hitboxCarro4Y2 = pBoxCar4.Location.Y + 50;
+            int hitboxCarro4X1 = pBoxCar4.Location.X, hitboxCarro4X2 = pBoxCar4.Location.X + 80;
+            bool avanzar = true, dead = false;
+            int hitboxRanaY1 = pBoxRana.Location.Y, hitboxRanaY2 = pBoxRana.Location.Y + 55;
+            int hitboxRanaX1 = pBoxRana.Location.X, hitboxRanaX2 = pBoxRana.Location.X + 80;
+            tmrCar4.Interval = 100 ;
+
+            if (avanzar)
+            {
+                pBoxCar4.Location = new Point(Carx4 + 5, Cary4);
+
+                if (Carx4 + 90 >= 1000)
+                {
+                    avanzar = false;
+                }
+            }
+            //hitbox4
+            if (hitboxRanaX1 <= hitboxCarro4X2)
+            {
+                if (hitboxRanaY1 <= hitboxCarro4Y2)
+                {
+                    if (hitboxRanaX2 >= hitboxCarro4X1)
+                    {
+                        if (hitboxRanaY2 >= hitboxCarro4Y2)
+                        {
+                            avanzar = false;
+                            dead = true;
+                        }
+                    }
+                }
+            }
+            //ciclo autos
+            if (avanzar == false)
+            {
+                if (dead == false)
+                {
+                    pBoxCar4.Location = new Point(-90, Cary4);
+                    avanzar = true;
+                }
+                else if (dead == true)
+                {
+                    tmrCar4.Enabled = false;
+                    pBoxRana.Image = Image.FromFile(@"C:\Users\johan\source\repos\Ranita rush\Ranita rush\Resources\Frogdead.png");
+                }
+            }
+            if (tmrCar4.Enabled == false)
+            {
+                tmrCar2.Enabled = false;
+                tmrCar3.Enabled = false;
+                tmrCar1.Enabled = false;
+                tmrCar5.Enabled = false;
+                tmrCar6.Enabled = false;
+            }
+        }
+
+        public void tmrCar5_Tick(object sender, EventArgs e)
+        {
+            int Carx5 = pBoxCar5.Location.X, Cary5 = pBoxCar5.Location.Y;
+
+            int hitboxCarro5Y1 = pBoxCar5.Location.Y, hitboxCarro5Y2 = pBoxCar5.Location.Y + 50;
+            int hitboxCarro5X1 = pBoxCar5.Location.X, hitboxCarro5X2 = pBoxCar5.Location.X + 80;
+            bool avanzar = true, dead = false;
+            int hitboxRanaY1 = pBoxRana.Location.Y, hitboxRanaY2 = pBoxRana.Location.Y + 55;
+            int hitboxRanaX1 = pBoxRana.Location.X, hitboxRanaX2 = pBoxRana.Location.X + 80;
+            tmrCar5.Interval = 2;
+
+            if (avanzar)
+            {
+                pBoxCar5.Location = new Point(Carx5 - 5, Cary5);
+
+                if (Carx5 <= 0)
+                {
+                    avanzar = false;
+                }
+            }
+            //hitbox5
+            if (hitboxRanaX1 <= hitboxCarro5X2)
+            {
+                if (hitboxRanaY1 <= hitboxCarro5Y2)
+                {
+                    if (hitboxRanaX2 >= hitboxCarro5X1)
+                    {
+                        if (hitboxRanaY2 >= hitboxCarro5Y2)
+                        {
+                            avanzar = false;
+                            dead = true;
+                        }
+                    }
+                }
+            }
+            //ciclo autos
+            if (avanzar == false)
+            {
+                if (dead == false)
+                {
+                    pBoxCar5.Location = new Point(900, Cary5);
+                    avanzar = true;
+                }
+                else if (dead == true)
+                {
+                    tmrCar5.Enabled = false;
+                    pBoxRana.Image = Image.FromFile(@"C:\Users\johan\source\repos\Ranita rush\Ranita rush\Resources\Frogdead.png");
+                }
+            }
+            if (tmrCar5.Enabled == false)
+            {
+                tmrCar2.Enabled = false;
+                tmrCar3.Enabled = false;
+                tmrCar4.Enabled = false;
+                tmrCar1.Enabled = false;
+                tmrCar6.Enabled = false;
+            }
+        }
+
+        public void tmrCar6_Tick(object sender, EventArgs e)
+        {
+            int Carx6 = pBoxCar6.Location.X, Cary6 = pBoxCar6.Location.Y;
+
+            int hitboxCarro6Y1 = pBoxCar6.Location.Y, hitboxCarro6Y2 = pBoxCar6.Location.Y + 50;
+            int hitboxCarro6X1 = pBoxCar6.Location.X, hitboxCarro6X2 = pBoxCar6.Location.X + 80;
+            bool avanzar = true, dead = false;
+            int hitboxRanaY1 = pBoxRana.Location.Y, hitboxRanaY2 = pBoxRana.Location.Y + 55;
+            int hitboxRanaX1 = pBoxRana.Location.X, hitboxRanaX2 = pBoxRana.Location.X + 80;
+            tmrCar6.Interval = 0001;
+
+            if (avanzar)
+            {
+                pBoxCar6.Location = new Point(Carx6 - 5, Cary6);
+
+                if (Carx6 <= 0)
+                {
+                    avanzar = false;
+                }
+            }
+            //hitbox6
+            if (hitboxRanaX1 <= hitboxCarro6X2)
+            {
+                if (hitboxRanaY1 <= hitboxCarro6Y2)
+                {
+                    if (hitboxRanaX2 >= hitboxCarro6X1)
+                    {
+                        if (hitboxRanaY2 >= hitboxCarro6Y2)
+                        {
+                            avanzar = false;
+                            dead = true;
+                        }
+                    }
+                }
+            }
+            //ciclo autos
+            if (avanzar == false)
+            {
+                if (dead == false)
+                {
+                    pBoxCar6.Location = new Point(900, Cary6);
+                    avanzar = true;
+                }
+                else if (dead == true)
+                {
+                    tmrCar6.Enabled = false;
+                    pBoxRana.Image = Image.FromFile(@"C:\Users\johan\source\repos\Ranita rush\Ranita rush\Resources\Frogdead.png");
+                }
+            }
+            if (tmrCar6.Enabled == false)
+            {
+                tmrCar2.Enabled = false;
+                tmrCar3.Enabled = false;
+                tmrCar4.Enabled = false;
+                tmrCar5.Enabled = false;
+                tmrCar1.Enabled = false;
+            }
+        }
+    }
+}
